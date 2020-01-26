c8c9e36d85dadfd3a19ba8fef83bfc73cd1e81f2
mykola.melnyk <nikolay.is.a.genius.dev@gmail.com>
2020-01-26 02:13:18 +0200

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.scss';
+
+export const App = () => {
+    return (
+        <div className={styles.app}>
+            <header className={styles.appHeader}>
+                header
+            </header>
+        </div>
+    );
+};
