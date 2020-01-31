13ccba45a9dc4ec57bf937150c48489d3a39369e
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-01-31 03:38:36 +0200

    "Site Setup" add table

--- src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
+++ src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
@@ -5,12 +5,31 @@ import React from 'react';
 import styles from './SiteSetupTable.module.scss';
 
 export const SiteSetupTable = ({keys, array}) => {
-    const value = Object.keys(array[keys]);
+    const values = Object.keys(array[keys]);
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.table}>
-                {value.map((item, index) => <div key={index}>{array[keys][item]}</div>)}
+
+
+                <div className={styles.tableHeader}>
+                    {values.map((item, index) => (
+                        <div key={index} className={styles.tableCell}>{item}</div>
+                    ))}
+                </div>
+                <div className={styles.tableBody}>
+
+                    {values.map((value, index) => {
+                            if (Array.isArray(array[keys][value])) return (
+                                <div key={index} className={styles.tableRow}>
+                                    {(array[keys][value].map((item, index) =>
+                                        <div key={index} className={styles.tableCell}>{item}</div>))}
+                                </div>)
+                        }
+                    )}
+                </div>
+
+
             </div>
         </div>
     );
