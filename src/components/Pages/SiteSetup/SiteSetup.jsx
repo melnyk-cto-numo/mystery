f09069165d5929dbd7de83bba8f95afd26673d90
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-01-31 16:15:38 +0200

    "Site Setup" connect with api

--- src/components/Pages/SiteSetup/SiteSetup.jsx
+++ src/components/Pages/SiteSetup/SiteSetup.jsx
@@ -31,37 +31,139 @@ const smallTable = [
 ];
 
 
+const headerTableName = [
+    {
+        faders: ['ON', 'Link', 'Name', 'Control No.'],
+        altFader1: ['ON', 'Link', 'Control No.'],
+        altFader2: ['ON', 'Link', 'Control No.'],
+        altToggle: ['ON', 'Link', 'Name', 'Control No.'],
+        meter1: ['ON', 'Link', 'Control No.'],
+        meter2: ['ON', 'Link', 'Control No.'],
+        mute: ['ON', 'Link', 'Name', 'Control No.'],
+        hg: ['ON', 'Link', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Link', 'On Name', 'Off Name', 'Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Control No.'],
+        altFader1: ['ON', 'Control No.'],
+        altFader2: ['ON', 'Control No.'],
+        altFader3: ['ON', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Control No.'],
+        mute: ['ON', 'Name', 'Control No.'],
+        meter1: ['ON', 'Control No.'],
+        meter2: ['ON', 'Control No.'],
+        hg: ['ON', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Control No.'],
+        altFader1: ['ON', 'Control No.'],
+        altFader2: ['ON', 'Control No.'],
+        altFader3: ['ON', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Control No.'],
+        mute: ['ON', 'Name', 'Control No.'],
+        meter1: ['ON', 'Control No.'],
+        meter2: ['ON', 'Control No.'],
+        hg: ['ON', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Control No.'],
+        altFader1: ['ON', 'Control No.'],
+        altFader2: ['ON', 'Control No.'],
+        altFader3: ['ON', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Control No.'],
+        mute: ['ON', 'Name', 'Control No.'],
+        meter1: ['ON', 'Control No.'],
+        meter2: ['ON', 'Control No.'],
+        hg: ['ON', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Control No.'],
+        altFader1: ['ON', 'Control No.'],
+        altFader2: ['ON', 'Control No.'],
+        altFader3: ['ON', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Control No.'],
+        mute: ['ON', 'Name', 'Control No.'],
+        meter1: ['ON', 'Control No.'],
+        meter2: ['ON', 'Control No.'],
+        hg: ['ON', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Name', 'Control No.'],
+        altFader1: ['ON', 'Link', 'Interface', 'Control No.'],
+        altFader2: ['ON', 'Link', 'Interface', 'Control No.'],
+        altFader3: ['ON', 'Link', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Link', 'Interface', 'Control No.'],
+        mute: ['ON', 'Name', 'Link', 'Interface', 'Control No.'],
+        meter1: ['ON', 'Link', 'Interface', 'Control No.'],
+        meter2: ['ON', 'Link', 'Interface', 'Control No.'],
+        hg: ['ON', 'Link', 'Interface', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Link', 'On Name', 'Off Name', 'Control No.'],
+    },
+    {
+        faders: ['ON', 'Link', 'Control No.'],
+        altFader1: ['ON', 'Control No.'],
+        altFader2: ['ON', 'Control No.'],
+        altFader3: ['ON', 'Control No.'],
+        altToggle: ['ON', 'Name', 'Control No.'],
+        mute: ['ON', 'Name', 'Control No.'],
+        meter1: ['ON', 'Control No.'],
+        meter2: ['ON', 'Control No.'],
+        hg: ['ON', 'Control No.'],
+        control: ['No', 'ON', 'Read Only', 'Link to Button', 'Name', 'Control No.'],
+    },
+];
+
 export const SiteSetup = () => {
     const [api, setApi] = useState(bbc);
     const [indicator, setIndicator] = useState(0);
+    const [headerTable, setHeaderTable] = useState(headerTableName[0]);
+    const [fieldId, setFieldId] = useState(true);
 
     useEffect(() => {
         switch (indicator) {
             case 0:
                 setApi(bbc);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 1:
                 setApi(hal);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 2:
                 setApi(jupiter);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 3:
                 setApi(qsys);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 4:
                 setApi(symetrix);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 5:
                 setApi(tesira);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             case 6:
                 setApi(xilica);
+                setHeaderTable(headerTableName[indicator]);
                 break;
             default:
                 setApi(bbc);
+                setHeaderTable(headerTableName[0]);
                 break;
         }
+        if (api) {
+            setFieldId(true);
+        } else {
+            setFieldId(false)
+        }
+
     }, [indicator]);
 
 
@@ -73,10 +175,18 @@ export const SiteSetup = () => {
                 </h2>
                 <Button text='Edit'/>
             </div>
-            <TableSmall fields={smallTable} setIndicator={setIndicator}/>
+            <div className={styles.siteSetupInner}>
+                <TableSmall fields={smallTable} setIndicator={setIndicator} fieldId={fieldId}/>
+                <div className={styles.siteSetupButtons}>
+                    <Button text='Save'/>
+                    <Button text='Download Backup'/>
+                    <Button text='Import to Page'/>
+                </div>
+            </div>
+
             {[api].map((data, index) => (
-                <Tabs key={index} className={styles.emSetupTabsWrapper}>
-                    <TabList className={styles.emSetupTabs}>
+                <Tabs key={index} className={styles.siteSetupTabsWrapper}>
+                    <TabList className={styles.siteSetupTabs}>
                         {Object.keys(data).map((keys, index) => {
                             if (keys !== 'DspType' && keys !== 'DspID' && keys !== 'SiteName') {
                                 return (<Tab key={index}>{keys}</Tab>)
@@ -86,10 +196,21 @@ export const SiteSetup = () => {
                     </TabList>
                     {Object.keys(data).map((keys, index) => {
                         if (keys !== 'DspType' && keys !== 'DspID' && keys !== 'SiteName') {
-                            return (
-                                <TabPanel key={index}>
-                                    <SiteSetupTable keys={keys} array={data}/>
+
+
+                            console.log(headerTableName[keys]);
+                            // console.log(keys)
+                            if (headerTable[keys]) {
+                                return (
+                                    <TabPanel key={index}>
+                                        <SiteSetupTable keys={keys} array={data} titles={headerTable[keys]}/>
+                                    </TabPanel>)
+                            } else {
+                                return (<TabPanel key={index}>
+                                    <SiteSetupTable keys={keys} array={data} titles={headerTable['faders']}/>
                                 </TabPanel>)
+                            }
+
                         }
                         return null;
                     })}
--- src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
+++ src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 //styles
 import styles from './SiteSetupTable.module.scss';
 
-export const SiteSetupTable = ({keys, array}) => {
+export const SiteSetupTable = ({keys, array, titles}) => {
     const values = Object.keys(array[keys]);
 
     return (
@@ -13,18 +13,30 @@ export const SiteSetupTable = ({keys, array}) => {
 
 
                 <div className={styles.tableHeader}>
-                    {values.map((item, index) => (
-                        <div key={index} className={styles.tableCell}>{item}</div>
+                    {titles.map((title, index) => (
+                        <div key={index} className={styles.tableCell}>{title}</div>
                     ))}
                 </div>
                 <div className={styles.tableBody}>
-
                     {values.map((value, index) => {
-                            if (Array.isArray(array[keys][value])) return (
-                                <div key={index} className={styles.tableRow}>
-                                    {(array[keys][value].map((item, index) =>
-                                        <div key={index} className={styles.tableCell}>{item}</div>))}
-                                </div>)
+                            if (Array.isArray(array[keys][value])) {
+                                return (
+                                    <div key={index} className={styles.tableRow}>
+                                        {(array[keys][value].map((item, index) =>
+                                            <div key={index} className={styles.tableCell}>
+                                                {value === 'enabled' ?
+                                                    <div className='checkbox'>
+                                                        {item ?
+                                                            <input id={'enabled_' + index} type="checkbox" checked
+                                                                   readOnly/> :
+                                                            <input id={'enabled_' + index} type="checkbox" readOnly/>}
+                                                        <label htmlFor={'enabled_' + index}/>
+                                                    </div> : value === 'link' ? (<a className='question' href='/'/>) :
+                                                        <span>{item}</span>}
+                                            </div>))}
+                                    </div>)
+                            }
+
                         }
                     )}
                 </div>
