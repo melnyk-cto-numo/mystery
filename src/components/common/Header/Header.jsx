98fde4060594881f3a98d6ef6008c24103148d7c
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-01-31 23:21:06 +0200

    responsive on mobile (< 992px)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Link} from 'react-router-dom';
 
@@ -9,20 +9,32 @@ import logo from '../../../assets/img/logo.png'
 import styles from './Header.module.scss';
 
 export const Header = () => {
+
+    const [active, setActive] = useState(false);
+    const addClass = () => {
+        setActive(!active);
+    };
+
     return (
-        <header className={styles.header}>
-            <Link to="/status" className={styles.logo}>
-                <div className={styles.logoText}>
-                    <h1>MYSTERY</h1>
-                    <p>Electronics.LLC</p>
-                </div>
-                <img src={logo} alt="mystery" className={styles.logoImage}/>
-            </Link>
-            <menu className={styles.headerMenu}>
-                <ul className={styles.headerList}>
-                    <MenuItem/>
-                </ul>
-            </menu>
+        <header className={styles.headerWrapper}>
+            <button type='button' className={active ? [styles.burgerMenu + ' ' + styles.active] : styles.burgerMenu}
+                    onClick={() => addClass()}>
+                <span className={styles.burgerMenuLines}/>
+            </button>
+            <div className={active ? [styles.header + ' ' + styles.active] : styles.header}>
+                <Link to="/status" className={styles.logo}>
+                    <div className={styles.logoText}>
+                        <h1>MYSTERY</h1>
+                        <p>Electronics.LLC</p>
+                    </div>
+                    <img src={logo} alt="mystery" className={styles.logoImage}/>
+                </Link>
+                <menu className={styles.headerMenu}>
+                    <ul className={styles.headerList}>
+                        <MenuItem/>
+                    </ul>
+                </menu>
+            </div>
         </header>
     );
 };
