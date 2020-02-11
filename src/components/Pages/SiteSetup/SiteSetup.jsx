3966706d03431a8772c9d9824c30b7ff1d3c293e
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-02-12 00:50:52 +0200

    tracking of "EM model"

--- src/components/Pages/SiteSetup/SiteSetup.jsx
+++ src/components/Pages/SiteSetup/SiteSetup.jsx
@@ -11,42 +11,66 @@ import {TableSmall} from "../../common";
 import {SiteSetupTable} from "./components/SiteSetupTable/SiteSetupTable";
 import {siteSetupActions} from "../../../bus/siteSetup/actions";
 import {errorsActions} from "../../../bus/errors/actions";
+import {statusActions} from "../../../bus/status/actions";
 import {getSiteSetup} from "../../../bus/siteSetup/selectors";
 import {getErrors} from "../../../bus/errors/selectors";
-
+import {getStatus} from "../../../bus/status/selectors";
 
 //styles
 import styles from './SiteSetup.module.scss';
 
 const headerTableName = [
     {
-        faders: ['Link', 'ON', 'Name', 'Control No.'],
-        altFader1: ['Link', 'ON', 'Control No.'],
-        altFader2: ['Link', 'ON', 'Control No.'],
-        altFader3: ['Link', 'ON', 'Control No.'],
-        altToggle: ['Link', 'ON', 'Invert', 'Control No.'],
-        mute: ['Link', 'ON', 'Invert', 'Control No.'],
-        meter1: ['Link', 'ON', 'Control No.'],
-        meter2: ['Link', 'ON', 'Control No.'],
-        hg: ['Link', 'ON', 'Control No.'],
-        control: ['Name', 'Link', 'ON', 'Read Only', 'Link to Button', 'Type', 'Control No.'],
+        faders: ['No', 'Bank', 'Link', 'ON', 'Name', 'Control No.'],
+        altFader1: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        altFader2: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        altFader3: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        altToggle: ['No', 'Bank', 'Link', 'ON', 'Invert', 'Control No.'],
+        mute: ['No', 'Bank', 'Link', 'ON', 'Invert', 'Control No.'],
+        meter1: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        meter2: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        hg: ['No', 'Bank', 'Link', 'ON', 'Control No.'],
+        control: ['Name', 'No', 'Bank', 'Link', 'ON', 'Read Only', 'Link to Button', 'Type', 'Control No.'],
     },
 ];
 
 export const SiteSetup = () => {
+    const [disabled, setDisabled] = useState(true);
+    const [bank, setBank] = useState('EM4');
+
     const dispatch = useDispatch();
     const siteSetup = useSelector(getSiteSetup);
     const errors = useSelector(getErrors);
+    const status = useSelector(getStatus);
     const data = siteSetup.siteSetup.data;
     const dataErrors = errors.errors.data;
-
-    const [disabled, setDisabled] = useState(true);
-
+    const model = status.status.data.model;
 
     useEffect(() => {
         dispatch(siteSetupActions.getSiteSetupAsync());
+        dispatch(statusActions.getStatusAsync());
         dispatch(errorsActions.getErrorsAsync());
     }, []);
+
+
+    useEffect(() => {
+
+        // check model for Bank field on Status page
+        switch (model) {
+            case 'EM4':
+                setBank(4);
+                break;
+            case 'EM8':
+                setBank(8);
+                break;
+            case 'EM12':
+                setBank(12);
+                break;
+            default:
+                setBank('model not found');
+                break;
+        }
+    }, [model]);
 
 
     if (data.DspType === undefined) {
@@ -105,7 +129,7 @@ export const SiteSetup = () => {
                                 return (
                                     <TabPanel key={index}>
                                         <SiteSetupTable keys={keys} array={item} titles={headerTableName[0][keys]}
-                                                        errors={dataErrors}
+                                                        errors={dataErrors} bank={bank}
                                                         disabled={disabled}/>
                                     </TabPanel>)
                             } else {
--- src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
+++ src/components/Pages/SiteSetup/components/SiteSetupTable/SiteSetupTable.jsx
@@ -9,17 +9,31 @@ import ok from '../../../../../assets/img/check.svg'
 import warning from '../../../../../assets/img/warning.svg'
 import error from '../../../../../assets/img/remove.svg'
 
-export const SiteSetupTable = ({keys, array, titles, errors, disabled = true}) => {
+
+export const SiteSetupTable = ({keys, array, titles, errors, bank, disabled = true}) => {
     const values = Object.keys(array[keys]);
+    const noAndBank = {
+        no: [],
+        banks: [],
+    };
 
     if (errors[keys] === undefined) {
         return false;
     }
 
+    // data for 2 column table
+    for (let b = 1; b <= 2 * bank; b++) {
+        noAndBank.no.push(b);
+        if (b <= bank) {
+            noAndBank.banks.push('A');
+        } else {
+            noAndBank.banks.push('B');
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.table}>
-
                 <div className={styles.siteSetupButtonsWrapper}>
                     {array[keys].type &&
                     <input type='text' className={styles.siteSetupButtons}
@@ -40,18 +54,25 @@ export const SiteSetupTable = ({keys, array, titles, errors, disabled = true}) =
                     {array[keys].name &&
                     <input type='text' className={styles.siteSetupButtons} defaultValue={`Name: ${array[keys].name}`}
                            disabled={disabled}/>}
-
                 </div>
-
                 <div className={styles.tableHeader}>
                     {titles.map((title, index) => (
                         <div key={index} className={styles.tableCell}>{title}</div>
                     ))}
                 </div>
                 <div className={styles.tableBody}>
+                    {/*add No adn Bank row*/}
                     <div className={styles.tableRow}>
-                        {/* errors array */}
-                        {(errors[keys].map((item, index) =>
+                        {(noAndBank.no.map((item, index) =>
+                            <div key={index} className={styles.tableCell}>{item}</div>))}
+                    </div>
+                    <div className={styles.tableRow}>
+                        {(noAndBank.banks.map((bank, index) =>
+                            <div key={index} className={styles.tableCell}>{bank}</div>))}
+                    </div>
+                    {/* errors array */}
+                    <div className={styles.tableRow}>
+                        {(errors[keys].slice(0, 2 * bank).map((item, index) =>
                             <div key={index} className={styles.tableCell}>
                                 <img src={item === 0 ? ok : item === 1 ? warning : error} alt=""/>
                             </div>))}
@@ -60,7 +81,7 @@ export const SiteSetupTable = ({keys, array, titles, errors, disabled = true}) =
                             if (Array.isArray(array[keys][value])) {
                                 return (
                                     <div key={index} className={styles.tableRow}>
-                                        {(array[keys][value].map((item, index) =>
+                                        {(array[keys][value].slice(0, 2 * bank).map((item, index) =>
                                             <div key={index} className={styles.tableCell}>
                                                 {value === 'enabled' ?
                                                     <div className='checkbox'>
