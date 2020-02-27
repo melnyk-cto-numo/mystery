68892e61888990370b553038563a89ba9c3a734a
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-02-27 02:05:25 +0200

    fix: disable "createLogger"

--- src/init/middleware/core.js
+++ src/init/middleware/core.js
@@ -1,21 +1,9 @@
-import {applyMiddleware, compose} from 'redux';
-import {createLogger} from 'redux-logger';
+import {applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 
-const loggerMiddleware = createLogger({
-    duration: true,
-    collapsed: true,
-    colors: {
-        title: () => '#139bfe',
-        error: () => '#ff0005',
-    },
-});
-
 const sagaMiddleware = createSagaMiddleware();
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const composeEnchancers = devtools || compose;
 
-const enhancedStore = composeEnchancers(applyMiddleware(sagaMiddleware, loggerMiddleware));
+const enhancedStore = applyMiddleware(sagaMiddleware);
 
 export {enhancedStore, sagaMiddleware};
